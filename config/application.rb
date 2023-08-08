680895bbdf5f0c3dca4a23ab0934b44e948bb727
Allyson Halley <allysonhalley@gmail.com>
2023-08-08 11:54:51 -0300

    tryin deploy, add command in application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module Kd1kApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # This was add to fix assets:precompile error.
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
